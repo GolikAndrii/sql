cf6f25297a74baa425f5fde5d7602d5f1a688d2e
Andrii Golik <golik.andrii@gmail.com>
2023-08-14 09:53:38 +0200

    Update homework_4.sql
    forthy

--- lesson_4/homework_4.sql
+++ lesson_4/homework_4.sql
@@ -29,9 +29,33 @@ LIMIT 3
 
 --3 Вывести названия категорий, в которых более 10 товаров
 
+SELECT
+	Categories.CategoryName,
+	COUNT(Products.ProductID) AS count_product_of_category
+FROM Products
 
+JOIN Categories ON Categories.CategoryID=Products.CategoryID
+
+GROUP BY Categories.CategoryID
+
+HAVING count_product_of_category >= 10
+
+ORDER BY count_product_of_category DESC
 -- ###############################################################################
 
 --4 Очистить тел/номер поставщикам из USA
+
+UPDATE Suppliers
+SET
+	Phone=''
+WHERE
+	Country="USA"
+-- ###############################################################################
+
+
 --5 Вывести имена и фамилии (и ко-во заказов) менеджеров, у которых ко-во заказов менее 15
+
+
+-- ###############################################################################
+
 --6 Вывести товар, который находится на втором месте по ко-ву заказов
